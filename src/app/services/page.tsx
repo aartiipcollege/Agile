51fdab95f1dd8dc25fd9fbe9181ddb356e337383
aartiipcollege <aarti.ggupta2004@gmail.com>
2025-09-09 18:07:48 +0530

    Remove breadcrumb and add no.

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -13,10 +13,7 @@ export const metadata: Metadata = {
 const ServicesPage = () => {
   return (
     <>
-      <Breadcrumb
-        pageName="Services Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
-      />
+   
       <WhatWeDoSection />
       <Feature />
       <WhyChooseUsSection />
